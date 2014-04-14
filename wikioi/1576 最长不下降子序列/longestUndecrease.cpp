32713e54584699e5f44510c6833b4d483fb95a2a
vinson0526 <vinson0526@gmail.com>
2014-04-14 20:38:25 +0800

    1576 80%

---
+++ wikioi/1576 最长不下降子序列/longestUndecrease.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int longest(int *arr, int n)
+{
+	int *len = new int[n];
+	int max = 0;
+	for(int i = 0; i != n; i++)
+	{
+		len[i] = 1;
+		for(int j = 0; j != i; j++)
+		{
+			if(arr[i] >= arr[j])
+			{
+				len[i] = len[i] > len[j] + 1 ? len[i] : len[j] + 1;
+			}
+		}
+		max = max > len[i] ? max : len[i];
+	}
+	return max;
+}
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	int *arr = new int[n];
+	for(int i = 0; i != n; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << longest(arr, n) << endl;
+	return 0;
+}
